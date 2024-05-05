961777e69c7b906ea8f1d2f41692bfb0f835114e
Erhan Kaya <erhankaya3470@gmail.com>
2024-05-06 00:49:29 +0300

    Player Attack
    Lightning attack attributes and animations added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,10 @@ public class PlayerController : MonoBehaviour
     private bool isDraggingItem = false;
     private GameObject draggedItem;
     [SerializeField] float moveSpeed = 1f;
-    private Vector3 lastMovementDirection = Vector3.right; 
+    [SerializeField] private GameObject lightningEffectPrefab;
+    [SerializeField] private float lightningEffectDuration = 0.5f;
+
+    private Vector3 lastMovementDirection = Vector3.right;
     private Rigidbody2D _playerRigidbody;
     private Animator _playerAnimator;
 
@@ -38,6 +41,10 @@ public class PlayerController : MonoBehaviour
 
     private void HandleMovement()
     {
+        //controlling of movement while player attacking
+        if (_playerAnimator.GetBool("isAttacking"))
+            return;
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
@@ -49,13 +56,14 @@ public class PlayerController : MonoBehaviour
         if (horizontalInput < 0f)
         {
             transform.localScale = new Vector3(-1, 1, 1);
-            lastMovementDirection = Vector3.left; 
+            lastMovementDirection = Vector3.left;
         }
         else if (horizontalInput > 0f)
         {
             transform.localScale = new Vector3(1, 1, 1);
-            lastMovementDirection = Vector3.right; 
+            lastMovementDirection = Vector3.right;
         }
+
         _playerRigidbody.MovePosition(newPosition);
         _playerAnimator.SetBool("isMoving", isMoving);
     }
@@ -70,16 +78,30 @@ public class PlayerController : MonoBehaviour
 
     private void HandleShooting()
     {
-        // Fare tıklaması ile ateş etme
         if (Input.GetMouseButtonDown(0))
         {
             Shoot();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _playerAnimator.SetBool("isAttacking", false);
         }
     }
 
     private void Shoot()
     {
-        // Ateş etme kodu
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f; 
+        float offset = 2.0f; 
+        Vector3 effectPosition = new Vector3(mousePosition.x, mousePosition.y + offset, mousePosition.z);
+        
+        GameObject lightningEffect = Instantiate(lightningEffectPrefab, effectPosition, Quaternion.identity);
+        
+        lightningEffect.SetActive(true);
+        Destroy(lightningEffect, lightningEffectDuration);
+
+        _playerAnimator.SetBool("isAttacking", true);
     }
 
     private void HandleInteraction()
